692de26207ab3a8cb7bd270db613cc30348a79c5
Lenin Zuna <55199721+Le-Zu@users.noreply.github.com>
2026-03-07 00:17:07 -0500

    Firebase setup + overrides (#30)

---
+++ warm-path/src/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeeK0SkwDULQeO8AgA0ClK8ZeuHt9ZOlM",
+  authDomain: "warmpath-c5e54.firebaseapp.com",
+  projectId: "warmpath-c5e54",
+  storageBucket: "warmpath-c5e54.firebasestorage.app",
+  messagingSenderId: "849586640507",
+  appId: "1:849586640507:web:8c1a96c49573956d9fb4de"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
