b79238e3fa03bc5a661615a9c074df6db5bfd2c0
Lenin Zuna <55199721+Le-Zu@users.noreply.github.com>
2026-03-07 18:01:18 -0500

    Backend Updates + Frontend Updates (#34)

---
+++ server/src/index.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import { verifyToken, AuthRequest } from './middleware/authMiddleware';
+
+const app = express();
+const PORT = process.env.port || 5000;
+
+// -- Standard Middleware --
+// Allows Vite fontend to make requests to this server
+app.use(cors());
+
+// Allows Express to read JSON data sent in the request body
+app.use(express.json());
+
+// -- Public route
+// Anyone can check, for if the server is running
+app.get('/api/ping', (req, res) => {
+    res.json({ message: 'PONG: WarmPath API is up and running!' });
+});
+
+// -- Protected route
+app.get('/api/test-auth', verifyToken, (req: AuthRequest, res) => {
+    
+    const userUid = req.user?.uid;
+    const userEmail = req.user?.email;
+
+    console.log('Secure request received from: ${userEmail}');
+
+    res.json({
+        message: "Authenticated request successful!",
+        userUid,
+        userEmail
+    });
+    })
+
+    // Starts the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
--- warm-path/src/App.jsx
+++ warm-path/src/App.jsx
@@ -1,11 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+
+// Import pages and components
+import HomePage from "./pages/HomePage"; // Page
+import LoginTest from "./pages/logintest"; // component
+//import ProtectRoute from './components/ProtectedRoute.jsx'; // component
 
 export default function App() {
    return (
+      // AuthProvider should wrap the Router so all routes have access to user state
       <BrowserRouter>
          <Routes>
+            {/* The Home Page */}
             <Route index element={<HomePage />} />
+
+            {/* The Login Test Page --Accessible to anyone (for now)*/}
+            <Route path="/logintest" element={<LoginTest />} />
+
+            {/* WIP Dashboard for later */}
+            {/* <Route path="/dashboard" element={
+               <ProtectRoute>
+                  <Dashboard />
+               </ProtectRoute>
+            } />
+         */}
          </Routes>
       </BrowserRouter>
    );
